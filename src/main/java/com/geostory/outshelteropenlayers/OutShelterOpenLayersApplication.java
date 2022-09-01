484a473c3bf5fbee489ffa93d3a0c3dfa89c79bc
Bohyun0320 <etoile0320@naver.com>
2022-09-01 16:52:30 +0900

    Initial commit

---
+++ src/main/java/com/geostory/outshelteropenlayers/OutShelterOpenLayersApplication.java
@@ -0,0 +1,13 @@
+package com.geostory.outshelteropenlayers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OutShelterOpenLayersApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OutShelterOpenLayersApplication.class, args);
+    }
+
+}
